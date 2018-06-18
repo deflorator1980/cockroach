6391d76ef881cfb185cd4a23bcbdc8e7d140dee0
AndreyIsakov <Andrey.Isakov@waveaccess.ru>
2018-06-18 12:20:32 +0300

    jpa

--- jpa/src/main/java/hello/Customer.java
+++ jpa/src/main/java/hello/Customer.java
@@ -1,46 +1,28 @@
 // tag::sample[]
 package hello;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Customer {
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-    private String firstName;
-    private String lastName;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String firstName;
+  private String lastName;
 
-    protected Customer() {}
-
-    public Customer(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "Customer[id=%d, firstName='%s', lastName='%s']",
-                id, firstName, lastName);
-    }
-
-// end::sample[]
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
+  public Customer(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
 
--- jpa/src/main/java/hello/CustomerRepository.java
+++ jpa/src/main/java/hello/CustomerRepository.java
@@ -1,10 +1,10 @@
 package hello;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
-    List<Customer> findByLastName(String lastName);
+  List<Customer> findByLastName(String lastName);
 }
