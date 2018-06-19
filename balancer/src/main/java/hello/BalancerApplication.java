8f73cefd65fa4dc879c690c3f4398fd76d30174a
Unknown <deflorator1980@yandex.ru>
2018-06-19 13:29:02 +0300

    all methods

--- balancer/src/main/java/hello/BalancerApplication.java
+++ balancer/src/main/java/hello/BalancerApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
@@ -28,8 +27,26 @@ public class BalancerApplication {
 
   @RequestMapping("/hi")
   public Object hi(@RequestParam(value="name", defaultValue="Artaban") String name) {
-
+    int a = 1;
     return this.restTemplate.getForObject("http://jparest/people", Object.class);
+  }
+
+  @RequestMapping("/hi/{id}")
+  public Object hiOne(@PathVariable int id) {
+    int a = 1;
+    return this.restTemplate.getForObject("http://jparest/people/" + id, Object.class);
+  }
+
+  @RequestMapping(value = "/hi", method = RequestMethod.POST)
+  public Object create(@RequestBody Object person) {
+    int a = 1;
+    return this.restTemplate.postForObject("http://jparest/people", person, Object.class);
+  }
+
+  @RequestMapping(value = "/hi/{id}", method = RequestMethod.DELETE)
+  public Object del(@PathVariable int id) {
+    int a =1;
+    return this.restTemplate.exchange("http://jparest/people/" + id, HttpMethod.DELETE, null, Object.class);
   }
 
   public static void main(String[] args) {
